c20c485d3e2860bfe443c0724ef0bd966f655cb1
hamed46 <47857182+hamed46@users.noreply.github.com>
2022-10-31 13:02:27 +0100

    Add files via upload

---
+++ src/main/java/tn/esprit/CallRestWebService.java
@@ -0,0 +1,41 @@
+package tn.esprit;
+
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+/**
+ * @author Walid-YAICH
+ * URL utiles :
+ * http://theoryapp.com/parse-json-in-java/
+ * https://jsonformatter.curiousconcept.com/
+ */
+
+public class CallRestWebService {
+
+	
+	public static final String endpoint = "http://ip-api.com/json";
+	//public static final String endpoint = "https://httpbin.org/get";
+	public static void main(String[] args) {
+		HttpClient client = new DefaultHttpClient();
+		HttpGet request = new HttpGet(endpoint);
+		String ip = "not found";
+		System.out.println("Hello");
+		try {
+			HttpResponse response = client.execute(request);
+			String jsonResponse = EntityUtils.toString(response.getEntity());
+			System.out.println("Response as String : " + jsonResponse);
+
+			//ip = responseObj.getString("origin");
+			System.out.println("ip : " + ip); 
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
+ 
